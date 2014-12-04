690af2b956a642c5f099483e614f7b2654dc85f9
Dayana-Marinova <dayana3264@gmail.com>
2014-12-04 03:59:47 +0200

    compled enumerable with test and everything

--- week1/3-my-enumerable/solution.rb
+++ week1/3-my-enumerable/solution.rb
@@ -27,39 +27,86 @@ module MyEnumerable
   end
 
   def each_cons(n)
-
+    result = []
+    each { |element| result << (element..element+(n-1)).to_a if element < size-n+2 }
+    result
   end
 
   def include?(element)
-    # Your code goes here.
+    (filter { |el| el == element }).size > 0 ? true : false
   end
 
   def count(element = nil)
-    # Your code goes here.
+    result = (filter { |i| i == element }).size
+    result > 0 ? result : size
   end
 
   def size
-    # Your code goes here.
+    reduce(0) { |sum, element| sum + 1 }
+  end
+
+  def group_by
+    result = {}
+    each { |element| result[yield(element)] ||= [] }
+    each { |element| result[yield(element)] << element}
+    result
+  end
+
+  def min
+    min_by
+  end
+
+  def min_by
+    min = "9999999999999999999999999999999999999999999999999999999999999999999"
+    if block_given?
+      each { |i| min = i if yield(i) < yield(min) }
+    else
+      each { |i| min = i if min.to_i > i }
+    end
+    min
+  end
+
+  def max
+    max_by
+  end
+
+  def max_by
+    max = ""
+    if block_given?
+      each { |i| max = i if yield(i) > yield(max) }
+    else
+      each { |i| max = i if max.to_i < i }
+    end
+    max
+  end
+
+  def minmax
+    minmax_by
+  end
+
+  def minmax_by(&block)
+    [min_by(&block), max_by(&block)]
+  end
+
+  def take(n)
+    result = []
+    each { |i| result << i if result.size < n }
+    result
+  end
+
+  def take_while
+    result = []
+    each { |i| result << i if yield(i) }
+    result
+  end
+
+  def drop(n)
+    each.to_a.reverse.take(size - n).reverse
+  end
+
+  def drop_while
+    result = []
+    each { |i| result << i unless yield(i) }
+    result
   end
 end
-
-class Collection
-  include MyEnumerable
-
-  def initialize(*data)
-    @data = data
-  end
-
-  def each(&block)
-    @data.each(&block)
-  end
-end
-
-coll = Collection.new(*(1..10))
-#p coll.map { |i| i*2 }
-#p coll.filter { |i| i > 4 }
-#p coll.reject { |i| i > 4 }
-#p coll.reduce(0) { |sum, i| sum + i }
-#p coll.any? { |i| i == 7 }
-#p coll.all? { |i| i < 11 }
-p
--- week1/3-my-enumerable/solution_test.rb
+++ week1/3-my-enumerable/solution_test.rb
@@ -43,5 +43,124 @@ class SolutionTest < Minitest::Test
     collection = Collection.new(*1..10)
 
     assert_equal true, collection.include?(5)
+    assert_equal false, collection.include?(11)
+  end
+
+  def test_any?
+    collection = Collection.new(*1..10)
+
+    assert_equal true, collection.any? { |i| i == 7 }
+    assert_equal false, collection.any? { |i| i < 0 }
+  end
+
+  def test_all?
+    collection = Collection.new *1..10
+
+    assert_equal true, collection.all? { |i| i < 11 }
+    assert_equal false, collection.all? { |i| i > 5}
+  end
+
+  def test_each_cons
+    collection = Collection.new *1..5
+
+    assert_equal [[1, 2, 3], [2, 3, 4], [3, 4, 5]], collection.each_cons(3)
+    assert_equal [[1, 2], [2, 3], [3, 4], [4, 5]], collection.each_cons(2)
+  end
+
+  def test_count
+    collection = Collection.new *1..10
+
+    assert_equal 1, collection.count(7)
+    assert_equal 10, collection.count(11)
+  end
+
+  def test_group_by
+    collection = Collection.new *1..10
+
+    result = {1=>[1, 4, 7, 10], 2=>[2, 5, 8], 0=>[3, 6, 9]}
+    result1 = {1=>[1, 3, 5, 7, 9], 0=>[2, 4, 6, 8, 10]}
+
+    assert_equal result, collection.group_by { |i| i%3 }
+    assert_equal result1, collection.group_by { |i| i%2 }
+  end
+
+  def test_size
+    collection = Collection.new *1..10
+
+    assert_equal 10, collection.size
+  end
+
+  def test_min
+    collection = Collection.new *1..10
+    collection1 = Collection.new 2, 2, 2, 1, 1, -11, 4, 4, 4, 4, - 1, -2, -10
+
+    assert_equal 1, collection.min
+    assert_equal -11, collection1.min
+  end
+
+  def test_min_by
+    collection = Collection.new *%w(albatross dog horse)
+
+    assert_equal "dog", collection.min_by { |x| x.length }
+  end
+
+  def test_max
+    collection = Collection.new *1..10
+    collection1 = Collection.new 2, 2, 2, 1, 1, -11, 4, 4, 4, 4, - 1, -2, -10
+
+    assert_equal 10, collection.max
+    assert_equal 4, collection1.max
+  end
+
+  def test_max_by
+    collection = Collection.new *%w(albatross dog horse)
+
+    assert_equal "albatross", collection.max_by { |x| x.length }
+  end
+
+  def test_minmax
+    collection = Collection.new *1..10
+    collection1 = Collection.new 2, 2, 2, 1, 1, -11, 4, 4, 4, 4, - 1, -2, -10
+
+    assert_equal [1, 10], collection.minmax
+    assert_equal [-11, 4], collection1.minmax
+  end
+
+  def test_minmax_by
+    collection = Collection.new *%w(albatross dog horse)
+
+    assert_equal ["dog", "albatross"], collection.minmax_by { |x| x.length }
+  end
+
+  def test_take
+    collection = Collection.new *1..10
+    collection1 = Collection.new 2, 2, 2, 1, 1, -11, 4, 4, 4, 4, - 1, -2, -10
+
+    assert_equal [1, 2, 3], collection.take(3)
+    assert_equal [2, 2, 2], collection1.take(3)
+  end
+
+  def test_take_while
+    collection = Collection.new *1..10
+    collection1 = Collection.new 2, 2, 2, 1, 1, -11, 4, 4, 4, 4, - 1, -2, -10
+
+    assert_equal [1, 2, 3], collection.take_while { |i| i < 4 }
+    assert_equal [-11, -1, -2, -10], collection1.take_while { |i| i < 0 }
+  end
+
+  def test_drop
+    collection = Collection.new *1..10
+    collection1 = Collection.new 2, 2, 2, 1, 1, -11, 4, 4, 4, 4, - 1, -2, -10
+
+    assert_equal [4, 5, 6, 7, 8, 9, 10], collection.drop(3)
+    assert_equal [1, 1, -11, 4, 4, 4, 4, - 1, -2, -10], collection1.drop(3)
+  end
+
+  def test_drop_while
+    collection = Collection.new *1..10
+    collection1 = Collection.new 2, 2, 2, 1, 1, -11, 4, 4, 4, 4, - 1, -2, -10
+
+    assert_equal [3, 4, 5, 6, 7, 8, 9, 10], collection.drop_while { |i| i < 3}
+    assert_equal [4, 4, 4, 4], collection1.drop_while { |i| i < 3}
   end
 end
